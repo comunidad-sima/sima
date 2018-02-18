e2b0266d54dd4e0acad8d547a8fa720eca74eddc
3126219340jJ <jean.suarez@cecar.edu.co>
2018-02-17 22:51:06 -0500

    Mi primer commit

---
+++ WebSima/WebSima/Controllers/InformeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace WebSima.Controllers
+{
+    public class InformeController : ApiController
+    {
+        public ActionResult Home()
+        {
+
+            return View();
+        }
+
+        private ActionResult View()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
