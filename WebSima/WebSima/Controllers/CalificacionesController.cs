2f83a87caf28a56534764b8a562e4ac86237a9a4
Fredys jose Vergara Cardenas <fredyjosevergaracardenas@gmail.com>
2018-03-28 17:32:24 -0500

    los docents puedene visualizar las notas registradas y actualizar o registrar nuevas

--- WebSima/WebSima/Controllers/CalificacionesController.cs
+++ WebSima/WebSima/Controllers/CalificacionesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebSima.Models;
+using WebSima.Models.WebApi;
 
 namespace WebSima.Controllers
 {
@@ -59,6 +60,91 @@ namespace WebSima.Controllers
                 return View("Grupos");
                 //return Redirect("~/Calificaciones/Grupos");
             }
+        }
+        [HttpPost]
+        public JsonResult CaligicacionesRegistro()
+        {
+            List<String[]> calificaciones = new List<String[]>();
+          
+            String asignatura = sesion.getMateria_nota();
+            String programa = sesion.getPrgrama_notas();
+            String grupo = sesion.getGrupo_nota();
+            String id_docente = sesion.getIdUsuario();
+            int id_calificacion = new MCalificaciones_periodo().getIdCalificacion(db, id_docente, programa, grupo, asignatura);
+            calificaciones_periodo calificaciones_periodo = db.calificaciones_periodo.Find(id_calificacion);
+            List<String> cabeza= new List<string>();
+            cabeza.Add("Identificación");
+            cabeza.Add("Nombre"); 
+            String[] notas ;   
+            List<EstudianteMateria> estudiantes= ConsumidorAppi.getPruebaEstudinates();
+            try
+            {
+                if (calificaciones_periodo != null)
+                {
+                    List<Notas> notas_registro = calificaciones_periodo.Notas.ToList();
+                    List<String> cabeza_tem = (from n in notas_registro select (n.tipo)).Distinct().ToList();
+                    List<String> idEstudiante_tem = (from n in notas_registro select (n.id_estudiante)).Distinct().ToList();
+                    idEstudiante_tem = idEstudiante_tem.Union(
+                        (from e in estudiantes
+                         where (e.nom_materia == asignatura && e.num_grupo == grupo && e.nom_unidad == programa)
+                         select (e.num_identificacion)).ToList()
+
+                        ).Distinct().ToList();
+
+
+                    cabeza = cabeza.Union(cabeza_tem).ToList();
+                    calificaciones.Add(cabeza.ToArray());
+                    double valorNota = 0;
+
+
+                    foreach (String id_ in idEstudiante_tem)
+                    {
+                        notas = new String[cabeza.Count()];
+                        notas[0] = id_;
+                        String nombre = "Nombre no disponible";
+                        var nom_estudiante=(from e in estudiantes
+                                         where (e.num_identificacion == id_)
+                                         select (e.nom_largo));
+                        if(nom_estudiante.Count()>0)
+                          nombre = (nom_estudiante).First();
+
+                        notas[1] = nombre;
+
+                        for (int i = 0; i < cabeza_tem.Count(); i++)
+                        {
+                            valorNota = 0;
+                            try
+                            {
+                                valorNota = (from n in notas_registro where (n.id_estudiante == id_ && n.tipo == cabeza_tem[i]) select (n.valor)).First();
+                            }
+                            catch (Exception) { }
+                            notas[i + 2] = "" + valorNota;
+                        }
+                        calificaciones.Add(notas);
+
+                    }
+                }
+                else
+                {
+                    calificaciones.Add(cabeza.ToArray());
+                    var estudiantesTemporal = (from e in estudiantes
+                                              where (e.nom_materia == asignatura && e.num_grupo == grupo && e.nom_unidad == programa)
+                                              select (e)).ToList();
+                    foreach (var e in estudiantesTemporal)
+                    {
+                        calificaciones.Add(new String[] { e.num_identificacion, e.nom_largo });
+
+                    }
+
+                    //se consultan los estudiantes
+                }
+            }
+            catch (Exception)
+            {
+                calificaciones.Add(cabeza.ToArray());
+                
+            }
+            return Json(calificaciones);
         }
 
         //
--- WebSima/WebSima/Models/MCalificaciones_periodo.cs
+++ WebSima/WebSima/Models/MCalificaciones_periodo.cs
@@ -11,7 +11,6 @@ namespace WebSima.Models
 {
     public class MCalificaciones_periodo
     {
-
         public string id_docente { get; set; }
         public int corte { get; set; }
         public string periodo { get; set; }
@@ -26,84 +25,106 @@ namespace WebSima.Models
 
         public String guardar(bd_simaEntitie db, FormCollection datos_notas)
         {
-            Sesion sesison = new Sesion();
+          
+            Sesion sesion = new Sesion();
             String guardado = "OK";
-            String asignatura = sesison.getMateria_nota();
-            String programa = sesison.getPrgrama_notas();
-            String grupo = sesison.getGrupo_nota();
-            String id_docente = sesison.getIdUsuario();
+            String asignatura = sesion.getMateria_nota();
+            String programa = sesion.getPrgrama_notas();
+            String grupo = sesion.getGrupo_nota();
+            String id_docente = sesion.getIdUsuario();
             try
             {
                 using (var transaccion = new TransactionScope())
                 {
                     using (var contestTransaccion = new bd_simaEntitie())
                     {
-                        eliminaCalificaciones(db, id_docente, programa, grupo, asignatura);
-                        calificaciones_periodo calificacion = new calificaciones_periodo
+                        // se comprueba que las columnas contenca una nota y las filas un estuniente  como minimo
+                        // se bene de restar los datos difrente a los de la tabla que vengan (datos_notas.AllKeys.ToList().Count()-1)
+
+                        if ((datos_notas.GetValues(0).Count() - 1 > 2) && (datos_notas.AllKeys.ToList().Count() - 1 > 2))
                         {
-                            asignatura = asignatura,
-                            corte=1,
-                            fecha_registro = DateTime.Now,
-                            grupo=grupo,
-                            id_docente=id_docente,
-                            periodo=MConfiguracionApp.getPeridoActual(db),
-                            programa=programa
-                            
-                        };
-                        db.calificaciones_periodo.Add(calificacion);
-                        db.SaveChanges();
-                        List<String> claves = datos_notas.AllKeys.ToList();
-                        int n=datos_notas.GetValues(0).Count();
-                        double valorNota = 0;
-                        NumberFormatInfo provider = new NumberFormatInfo();
-                        provider.NumberDecimalSeparator = ",";
-                        /// se recorren las filas de la tabla
-                        List<String> cabezaTabla = datos_notas.GetValues(0).ToList();
-                        for (int i = 1; i < claves.Count() -1; i++)
-                        {                            
-                             List<String> dato = datos_notas.GetValues(claves[i]).ToList();
-                             String valida = validaFila(cabezaTabla.Count(), dato, provider);
-                             if (valida.Equals("OK")) { 
-                            /// se recorren las columnas
-                                 for (int j = 1; j < n - 1; j++)
-                                 {
-                                     // posicion dende esta el nombre, esto no se guarda
-                                     if (j != 1)
-                                     {
-                                         valorNota = Convert.ToDouble(dato[j], provider);
-                                         Notas nota = new Notas
-                                         {
-                                             id_calificaciones_periodo = calificacion.id,
-                                             id_estudiante = dato[0],
-                                             tipo = cabezaTabla[j],
-                                             valor = valorNota
-                                         };
-                                         db.Notas.Add(nota);
-                                         db.SaveChanges();
-                                     }
-                                 }
-                             }
-                             else
-                             {
-                                 guardado = valida;
-                                 break;
-                             }
-                        }
-                        if (guardado.Equals("OK"))
-                        {
-                            MAlerta alerta = new MAlerta
+
+                            List<String> cabezaTabla = datos_notas.GetValues(0).ToList();
+                            // no se permite q el nombre de las actividades sean las misma
+                            if (cabezaTabla.Distinct().Count() == cabezaTabla.Count())
                             {
-                                creador = "SIMA",
-                                eliminada = 0,
-                                fecha_creada = DateTime.Now,                                
-                                mensaje = "Actualización de calificaciones del estudiante ",
-                                perfil_ver = "Administrador",
-                                tipo_alerta = "Notas",
-                                titulo = "CALIFICACIONES "+asignatura,
-                                vista = 0
-                            };
-                            alerta.crearAlerta(db, alerta);
-                            transaccion.Complete();
+                                eliminaCalificaciones(db, id_docente, programa, grupo, asignatura);
+                                calificaciones_periodo calificacion = new calificaciones_periodo
+                                {
+                                    asignatura = asignatura,
+                                    corte = 1,
+                                    fecha_registro = DateTime.Now,
+                                    grupo = grupo,
+                                    id_docente = id_docente,
+                                    periodo = MConfiguracionApp.getPeridoActual(db),
+                                    programa = programa
+
+                                };
+                                db.calificaciones_periodo.Add(calificacion);
+                                db.SaveChanges();
+                                List<String> claves = datos_notas.AllKeys.ToList();
+                                //  cabeceras tabla
+                                int n = datos_notas.GetValues(0).Count();
+                                double valorNota = 0;
+                                NumberFormatInfo provider = new NumberFormatInfo();
+                                provider.NumberDecimalSeparator = ",";
+                                /// se recorren las filas de la tabla
+
+                                // si se envian otros valores en el formulario, se debe de restar en el  (claves.Count()-n) del primer for
+                                // siempre se  suma -1 porque la ultima fila siempre esta vacia
+                                // se inicia en 1 porque la primera fila es la que contiene el nombre de las actividades
+                                for (int i = 1; i < claves.Count() - 1; i++)
+                                {
+                                    List<String> dato = datos_notas.GetValues(claves[i]).ToList();
+                                    guardado = validaFila(cabezaTabla.Count(), dato, provider);
+                                    if (guardado.Equals("OK"))
+                                    {
+                                        /// se recorren las columnas, las dos primeras columnas no se toman 
+                                        /// //se suma -1 porque la ultma columna siempre esta vacia
+                                        for (int j = 2; j < n - 1; j++)
+                                        {
+                                            valorNota = Convert.ToDouble(dato[j], provider);
+                                            Notas nota = new Notas
+                                            {
+                                                id_calificaciones_periodo = calificacion.id,
+                                                id_estudiante = dato[0],
+                                                tipo = cabezaTabla[j],
+                                                valor = valorNota
+                                            };
+                                            db.Notas.Add(nota);
+                                            db.SaveChanges();
+                                        }
+                                    }
+                                    else
+                                    {
+                                        break;
+                                    }
+                                }
+                                if (guardado.Equals("OK"))
+                                {
+                                    MAlerta alerta = new MAlerta
+                                    {
+                                        creador = "SIMA",
+                                        eliminada = 0,
+                                        fecha_creada = DateTime.Now,
+                                        mensaje = "Actualización de calificaciones del estudiante ",
+                                        perfil_ver = "Administrador",
+                                        tipo_alerta = "Notas",
+                                        titulo = "CALIFICACIONES " + asignatura,
+                                        vista = 0
+                                    };
+                                    alerta.crearAlerta(db, alerta);
+                                    transaccion.Complete();
+                                }
+                            }
+                            else
+                            {
+                                guardado = "El nombre de las actividades no pueden ser el mismo.";
+                            }
+                        }
+                        else
+                        {
+                            guardado = "Se debe de registrar como minimo una nota y un estunate antes de guardar.";
                         }
                     }
                 }
@@ -142,11 +163,9 @@ namespace WebSima.Models
             }catch(Exception ){
                 valido = "Nota invalida ["+nota+"]";
             }
-
             return valido;
-
         }
-
+        
         public void eliminaCalificaciones(bd_simaEntitie db,String id_docente,String programa,String grupo,String materia){
 
             var calificacion = (from c in db.calificaciones_periodo where(c.grupo==grupo && c.programa==programa &&
@@ -164,6 +183,20 @@ namespace WebSima.Models
                 db.calificaciones_periodo.Remove(califi);
                 db.SaveChanges();
             }
+
+        }
+        public int getIdCalificacion(bd_simaEntitie db, String id_docente, String programa, String grupo, String materia)
+        {
+            int id = -1;
+            var calificacion = (from c in db.calificaciones_periodo
+                                where (c.grupo == grupo && c.programa == programa &&
+                                    c.asignatura == materia && c.id_docente == id_docente)
+                                select c);
+            if (calificacion.Count() > 0)
+            {
+                id = calificacion.First().id;
+            }
+            return id;
 
         }
     }
